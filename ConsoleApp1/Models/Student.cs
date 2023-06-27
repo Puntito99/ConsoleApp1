08dafe3f1ca837c54f072d13899b634415298810
Puntito99 <PIERINEX05@gmail.com>
2023-06-26 20:07:05 -0500

    Agregar archivos de proyecto.

---
+++ ConsoleApp1/Models/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Models
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Direccion { get; set; }
+        public string Telefono { get; set; }
+        public string FechaNac { get; set; }
+        public string Estado { get; set; }
+    }
+}
